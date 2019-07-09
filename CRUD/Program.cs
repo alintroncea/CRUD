92430f5c07b65f31269b2e01f3a28647b6e495ac
thisisalin <alintroncea@gmail.com>
2019-07-09 16:39:33 +0300

    7/9

--- CRUD/Program.cs
+++ CRUD/Program.cs
@@ -8,28 +8,6 @@ namespace CRUD
     {
         static void Main()
         {
-            SortedList<int> sortedList = new SortedList<int>();
-#pragma warning disable S109 // Magic numbers should not be used
-            sortedList.Add(9);
-            sortedList.Add(5);
-            sortedList.Add(7);
-            sortedList.Add(3);
-#pragma warning restore S109 // Magic numbers should not be used
-
-            foreach (int i in sortedList)
-            {
-                Console.WriteLine(i);
-            }
-
-            Console.WriteLine("----------------");
-#pragma warning disable S109 // Magic numbers should not be used
-            sortedList.SetValue(1, 7);
-#pragma warning restore S109 // Magic numbers should not be used
-
-            foreach (int i in sortedList)
-            {
-                Console.WriteLine(i);
-            }
         }
     }
 }
